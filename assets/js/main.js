685531e41ef261e816ef7cab5c9914bdc6fc9a60
Orla Madden <omadden2@gmail.com>
2020-03-04 21:07:13 +0000

    added id to one answer button to test output of name of current sprite pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ function fetchPokemon() {
    .then(response => response.json())
    .then(data => {
    document.getElementById('pokeImg').innerHTML = `<img src="${data.sprites.front_default}" height="250" width="250">`;
+   document.getElementById('answer1').innerHTML = data.name.toUpperCase();
 })
   .catch((error) => console.log(error))
 }
