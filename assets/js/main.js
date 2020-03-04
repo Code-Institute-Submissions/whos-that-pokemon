40aba35f9c75868976b1a4374f904e3a6a6b6701
Orla Madden <omadden2@gmail.com>
2020-03-04 21:01:34 +0000

    initialise pokeapi fetch url, fetched sprites to the DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const number = 68;
+const url = `https://pokeapi.co/api/v2/pokemon/${number}`;
+
+function fetchPokemon() {
+
+  fetch(url)
+   .then(response => response.json())
+   .then(data => {
+   document.getElementById('pokeImg').innerHTML = `<img src="${data.sprites.front_default}" height="250" width="250">`;
+})
+  .catch((error) => console.log(error))
+}
+  
+fetchPokemon();
