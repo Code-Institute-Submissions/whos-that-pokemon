27095f5a1fa59f24bc34c0f671d37f9929a597b9
Orla Madden <omadden2@gmail.com>
2020-03-06 07:07:15 +0000

    removed external api data and replaced with local for testing and practice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // setting DOM elements and url for fetching
 const number = 4;
-const url = `https://pokeapi.co/api/v2/pokemon/${number}`;
+const url = `assets/js/pokemon-test.json`;
 const pokeImage = document.getElementById('pokeImg');
 const pokeName = document.getElementsByClassName('poke-answer');
 //console.log(pokeName[1].innerHTML);
@@ -11,11 +11,10 @@ let questionCounter = 0;
 
 function fetchPokemon() {
 
-  fetch(url)
+  fetch('assets/js/pokemon-test.json')
    .then(response => response.json())
    .then(pokeData => {
-   pokeImage.innerHTML = `<img src="${pokeData.sprites.front_default}" height="250" width="250">`;
-   pokeName.innerHTML = pokeData.name.toUpperCase();
+   pokeImage.innerHTML = `<img src="${pokeData[0].sprites.front_default}" height="250" width="250">`;
 })
   .catch((error) => console.log(error))
 }
