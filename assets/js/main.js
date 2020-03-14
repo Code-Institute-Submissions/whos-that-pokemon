47201ac71dca51a5320905ac04f2f16360e49c76
Orla Madden <omadden2@gmail.com>
2020-03-14 19:34:42 +0000

    removed unused functions, replaced with fetchPokemon() function that calls API

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,22 +12,22 @@ let questionCounter = 0;
 const questionMax = 10;
 let availableMon = [];
 
-function startQuiz() {
-  questionCounter = 0;
-  score = 0;
-  availableMon = [...pokeQuestions];
-  console.log(availableMon);
-  getNewPokemon();
+function fetchPokemon() {
+  const promises = [];
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+}
+
+Promise.all(promises).then((results) => {
+  const pokemon = results.map((result) => ({
+      name: result.name,
+      image: result.sprites.front_default
+  }));
+  pokeData = shuffleMon(pokemon); // shuffles Pokemon data
+  pushMonToDOM(); // pushes Pokemon information to the DOM
+  console.log(pokeData);
+});
 };
 
-function getNewPokemon() {
-  const pokeIndex = Math.floor(Math.random() * availableMon.length);
-  currentMon = availableMon[pokeIndex];
-  pokeImage.innerHTML = `<img src="${currentMon.sprites.front_default}" height="250" width="250">`;
-
-};
-
-startQuiz();
-
-// resets the game
-resetQuiz();
+fetchPokemon();
